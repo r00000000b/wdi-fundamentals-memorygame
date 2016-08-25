51b54719f0117d927ff8b318535e98ecc995497b
Roberto Ching <robertoching@Robertos-MacBook-Pro.local>
2016-08-26 04:04:23 +0800

    added Javascript conditionals

--- prework/main.js
+++ prework/main.js
@@ -2,3 +2,11 @@ var card1 = "queen";
 var card2 = "queen";
 var card3 = "king";
 var card4 = "king";
+
+if (card1 === card2) {
+	alert("You found a match!");
+}	else if (card3 === card4) {
+	alert("You found a match!");
+}	else {
+		alert("Sorry, try again!");
+}
